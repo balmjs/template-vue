6a9396040d42e7f13f81c73a2b8e9066492e1693
elf-mouse <ifmouz@gmail.com>
2017-01-07 20:57:01 +0800

    first commit

---
+++ templates/app/js/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <img :src="logo" alt="">
+    <hello></hello>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello';
+
+export default {
+  name: 'app',
+  components: {
+    Hello
+  },
+  data() {
+    return {
+      logo: require('../images/logo.png')
+    };
+  }
+};
+</script>
---
+++ templates/gulpfile.js
@@ -0,0 +1,45 @@
+var balm = require('balm');
+var config = require('./balmrc');
+
+balm.config = {
+  server: {
+    open: true,
+    proxyTable: config.proxyTable
+  },
+  roots: {
+    source: 'app'
+  },
+  paths: {
+    source: {
+      css: 'css',
+      js: 'js',
+      img: 'images'
+    }
+  },
+  styles: {
+    ext: 'scss'
+  },
+  scripts: {
+    entry: config.entry,
+    loaders: [{
+      test: /\.vue$/,
+      loader: 'vue'
+    }],
+    alias: {
+      vue: 'vue/dist/vue.js'
+    }
+  },
+  cache: true,
+  assets: config.assets
+};
+
+balm.go(function(mix) {
+  if (balm.config.production) {
+    // for static
+    mix.publish();
+    // for html
+    Object.keys(config.publish).forEach(function(key) {
+      mix.publish(key, config.publish[key].target, config.publish[key].option || {});
+    });
+  }
+});
