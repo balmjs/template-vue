ec39dd75f3872f1b5a683e18c83a7e612077ef49
Elf-mousE <ifmouz@gmail.com>
2018-10-08 10:12:50 +0800

    update

--- templates/.eslintrc.js
+++ templates/.eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
   plugins: ['vue'],
   // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
   // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-  extends: ['plugin:vue/essential'],
+  extends: ['plugin:vue/essential', 'eslint:recommended'],
   // add your custom rules here
   rules: {
     // allow debugger during development
