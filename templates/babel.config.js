c1be9d3aaddb997391b584d0997bda062ad750bc
Elf-mousE <ifmouz@gmail.com>
2020-01-29 08:15:03 +0800

    update balm@2.3.0

--- templates/babel.config.js
+++ templates/babel.config.js
@@ -7,29 +7,5 @@ module.exports = {
       }
     ]
   ],
-  plugins: [
-    // Runtime
-    '@babel/plugin-transform-runtime',
-    // Stage 2
-    [
-      '@babel/plugin-proposal-decorators',
-      {
-        legacy: true
-      }
-    ],
-    '@babel/plugin-proposal-function-sent',
-    '@babel/plugin-proposal-export-namespace-from',
-    '@babel/plugin-proposal-numeric-separator',
-    '@babel/plugin-proposal-throw-expressions',
-    // Stage 3
-    '@babel/plugin-syntax-dynamic-import',
-    '@babel/plugin-syntax-import-meta',
-    [
-      '@babel/plugin-proposal-class-properties',
-      {
-        loose: false
-      }
-    ],
-    '@babel/plugin-proposal-json-strings'
-  ]
+  plugins: ['@babel/plugin-transform-runtime']
 };
