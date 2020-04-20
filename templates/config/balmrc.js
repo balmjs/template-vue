5cf723acc2f92c7632e264d454184f2d00fd22ff
Elf-mousE <ifmouz@gmail.com>
2020-04-20 10:35:35 +0800

    update

--- templates/config/balmrc.js
+++ templates/config/balmrc.js
@@ -6,14 +6,13 @@ const VueLoaderPlugin = require('vue-loader/lib/plugin');
 // 中文文档 - https://balmjs.com/docs/v2/zh/config/
 module.exports = {
   server: {
-    open: true,
-    proxyConfig: {
-      context: '/api',
-      options: {
-        target: 'http://your.project.dev', // Target host
-        changeOrigin: true // Needed for virtual hosted sites
-      }
-    }
+    // proxyConfig: {
+    //   context: '/api',
+    //   options: {
+    //     target: 'http://your.project.dev', // Target host
+    //     changeOrigin: true // Needed for virtual hosted sites
+    //   }
+    // }
   },
   roots: {
     source: 'app'
