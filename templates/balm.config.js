c42f5f17c5012c6937fb9e891cc93f0c5aa811e6
Elf-mousE <ifmouz@gmail.com>
2020-08-03 16:11:57 +0800

    update balm@3

--- templates/balm.config.js
+++ templates/balm.config.js
@@ -1,10 +1,14 @@
 /* eslint-env node */
-const balm = require('balm');
 const config = require('./config/balmrc');
 const publish = require('./config/balm.publish');
 
-balm.config = config;
-
-balm.go(mix => {
+const api = mix => {
   publish(mix);
-});
+};
+
+module.exports = () => {
+  return {
+    config,
+    api
+  };
+};
